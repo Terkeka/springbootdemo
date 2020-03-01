3189708c903e57c5a64b183c184dcc17623bd846
Terkeka <sulimansaadeldin@gmail.com>
2020-03-01 09:45:59 -0600

    Added all project files

---
+++ course-api/src/main/java/io/javasms/springboot/HelloController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package io.javasms.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ThinkPad T540p
+ *
+ */
+@RestController
+public class HelloController {
+   
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+	}
+}
